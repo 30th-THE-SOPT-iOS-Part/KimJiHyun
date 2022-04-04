f8edb9ec4c0c7c6c5e8f5b4605f98b3dcb4a8b77
Jihyun247 <wlguslove12345@gmail.com>
2022-04-04 23:59:27 +0900

    유아이

---
+++ Week1/1stAssignment/1stAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1stAssignment
+//
+//  Created by 김지현 on 2022/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
