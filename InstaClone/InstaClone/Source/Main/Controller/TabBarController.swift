4777603de6a0adb2595386d8628f17fe97db3acc
Jihyun247 <wlguslove12345@gmail.com>
2022-04-22 14:49:30 +0900

    :recycle: 불필요한 코드 삭제
    불필요한 코드 삭제

--- InstaClone/InstaClone/Source/Main/Controller/TabBarController.swift
+++ InstaClone/InstaClone/Source/Main/Controller/TabBarController.swift
@@ -7,12 +7,7 @@
 
 import UIKit
 
-import RxSwift
-import RxCocoa
-
 final class TabBarController: UITabBarController {
-    
-    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
